814b15af8104f64bc00ce4ce68cb9b5bc8170e67
J4m3s00 <james.lol2000@gmail.com>
2020-05-28 00:37:06 +0200

    glff test

--- main.cpp
+++ main.cpp
@@ -1,8 +1,39 @@
 #include <iostream>
+#include <GLFW/glfw3.h>
 
 int main(int argc, char const *argv[])
 {   
+    GLFWwindow* window;
 
-    std::cout << "Hello World!" << std::endl;
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    std::cout << "Hello World!" << std::endl; 
     return 0;
 }
