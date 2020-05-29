03cf02430c8ec973d9eaecac9b4c225a79b7de89
J4m3s00 <james.lol2000@gmail.com>
2020-05-29 19:51:09 +0200

    ref

---
+++ include/graphics/engine_window.h
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace Engine {
+
+    class EngineWindow
+    {
+    public:
+        EngineWindow()
+    };
+
+}
--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 #include <GLFW/glfw3.h>
 
+void GL_ERROR()
+{
+    GLenum err = glGetError();
+    if (err != GL_NO_ERROR)
+    {
+        std::cout << glGetString(err) << std::endl;
+    }
+}
+
 int main(int argc, char const *argv[])
 {   
     GLFWwindow* window;
@@ -17,9 +26,30 @@ int main(int argc, char const *argv[])
         return -1;
     }
 
+
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    float position[] = {
+        -0.5f, -0.5f, 
+         0.0f,  0.5f,
+         0.5f, -0.5f
+    };
+
+    int indicies[] = {
+        0, 1, 2
+    };
+
+
+
+    unsigned int vbo, ibo;
+    glGenBuffers(1, &vbo);
+    GL_ERROR();
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    GL_ERROR();
+
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), position, GL_STATIC_DRAW);
+    GL_ERROR();
     std::cout << glGetString(GL_VERSION) << std::endl;
 
     /* Loop until the user closes the window */
@@ -28,12 +58,11 @@ int main(int argc, char const *argv[])
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBegin(GL_TRIANGLES);
-        glVertex2f(-0.5f, -0.5f);
-        glVertex2f(-0.0f,  0.5f);
-        glVertex2f( 0.5f, -0.5f);
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
         
-        glEnd();
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
