00cdc260b2e46ef798dd2c492f84d59420492648
valentinstoecker <33518110+valentinstoecker@users.noreply.github.com>
2020-06-07 21:48:37 +0200

    fix small stuff

--- include/Engine.h
+++ include/Engine.h
@@ -24,7 +24,8 @@ using EVector = std::vector<T>;
 //------------------------------------------------------------------
 // Extern Dependencies
 
-#include <glad/glad.h>
+
+#include "glad/glad.h"
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 #include <glm.hpp>
